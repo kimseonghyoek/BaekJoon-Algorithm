0c712cc60f8e7eb7539c76c63a96f18f4eb67a27
Kimseonghyoek <cilo1223@naver.com>
2022-03-04 03:19:31 +0900

    add arithmetic

---
+++ arithmetic.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a = 0;
+	int b = 0;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d", a % b);
+
+	return 0;
+}
