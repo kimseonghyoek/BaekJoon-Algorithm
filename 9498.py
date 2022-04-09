d87e502481acb796d097487655f00b16cb17d2ed
Kimseonghyoek <cilo1223@naver.com>
2022-04-10 02:34:54 +0900

    Add 9498py

---
+++ 9498.py
@@ -0,0 +1,12 @@
+input = int(input())
+
+if input >= 90:
+    print("A")
+elif input >= 80:
+    print("B")
+elif input >= 70:
+    print("C")
+elif input >= 60:
+    print("D")
+else :
+    print("F")
